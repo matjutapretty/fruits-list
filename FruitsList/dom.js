18e2ae5ae57fa89ef59a7e56d83ad7251dbde37a
Pretty <mphefopretty1997@gmail.com>
2022-03-08 21:18:01 +0200

    added the tests

--- FruitsList/dom.js
+++ FruitsList/dom.js
@@ -1,6 +1,6 @@
 const values = ["Grapes 🍇", "Melon 🍈", "Watermelon 🍉", "Tangerine 🍊", "Lemon 🍋", "Banana 🍌", "Pineapple 🍍", "Mango 🥭", "Red Apple 🍎"];
-window.localStorage.setItem('values', JSON.stringify(values));
-JSON.parse(window.localStorage.getItem('values'));
+// window.localStorage.setItem('values', JSON.stringify(values));
+// JSON.parse(window.localStorage.getItem('values'));
 
 
 // for adding new fruit
@@ -27,6 +27,17 @@ button.onclick = function() {
   }
 
   input.focus();
+
+  function isEmoji(str) {
+    var ranges = [
+        '(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])' // U+1F680 to U+1F6FF
+    ];
+    if (str.match(ranges.join('|'))) {
+        return true;
+    } else {
+        return false;
+    }
+}
 }
 
 function sort() {
@@ -68,11 +79,13 @@ function reverse() {
   while (run) {
       run = false;
       li = values.getElementsByTagName("LI");
-console.log(li)
-li = Array.from(li).reverse()
-console.log(li)
+      Array.from(values.children).sort().reverse().forEach(element =>values.appendChild(element));
+      
       // Loop traversing through all the list items
-      for (i = 0; i < (li.length - 1); i++) {
+        for( i = li.length-1; i >= 0; --i) {
+        values.appendChild(li[i]);
+    
+      // for (i = 0; i < (li.length - 1); i++) {
           stop = false;
           if (li[i].innerHTML.toLowerCase() > 
               li[i + 1].innerHTML.toLowerCase()) {
@@ -113,4 +126,5 @@ console.log(li)
 
  
     document.getElementById("container");
+    
 
---
+++ FruitsList/factory-functions.js
@@ -0,0 +1,43 @@
+function fruits_list () {
+  let values = ["Grapes 🍇", "Melon 🍈", "Watermelon 🍉", "Tangerine 🍊", "Lemon 🍋", "Banana 🍌", "Pineapple 🍍", "Mango 🥭", "Red Apple 🍎"];
+
+// button.onclick = function() {
+// }
+
+function sort() {
+  values = document.getElementById("values");
+}
+
+function reverse() {
+  values = document.getElementById("values");
+}
+
+    function myFunction(){
+      // let input, filter, ul, li, a, i, txtValue;
+      //   input = document.getElementById('myInput');
+      //   filter = input.value.toUpperCase();
+      //   ul = document.getElementById("values");
+      //   li = ul.getElementsByTagName('li');
+      
+      //   // Loop through all list items, and hide those who don't match the search query
+      //   for (i = 0; i < li.length; i++) {
+      //     a = li[i].getElementsByTagName("a")[0];
+      //     txtValue = a.textContent || a.innerText;
+      //     if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      //       li[i].style.display = "";
+      //     } else {
+      //       li[i].style.display = "none";
+      //     }
+      //   }
+    }
+
+    return {
+        myFunction,
+        reverse,
+        sort,
+
+    }
+}
+
+    
+
